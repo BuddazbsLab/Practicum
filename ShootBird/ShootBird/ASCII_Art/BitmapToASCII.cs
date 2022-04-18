97d51cf8dbd1527dd6e4becf7629728517e84b91
иван <buddazbs@yandex.ru>
2022-04-18 16:41:35 +0300

    Добавлен конвертер картинок в символы

---
+++ ShootBird/ShootBird/ASCII_Art/BitmapToASCII.cs
@@ -0,0 +1,38 @@
+﻿using System.Drawing;
+
+namespace ASCII_Art
+{
+    internal class BitmapToASCII
+    {
+        private readonly char[] _charsTables = { '.', ',', ':', '+', '*', '?', '%', '$', '#', '@' };
+        private readonly Bitmap _bitmap;
+        public BitmapToASCII(Bitmap bitmap)
+        {
+            _bitmap = bitmap;
+        }
+
+        public char[][] Convert()
+        {
+            var result = new char[_bitmap.Height][];
+
+            for(int i = 0; i < _bitmap.Height; i++)
+            {
+                result[i] = new char[_bitmap.Width];
+
+                for(int j = 0; j < _bitmap.Width; j++)
+                {
+                    int mapIndex = (int)Map(_bitmap.GetPixel(j, i).R, 0, 255, 0, _charsTables.Length - 1);
+                    result[i][j] = _charsTables[mapIndex];
+                }
+            }
+
+            return result;
+        }
+
+
+        private float Map(float valueToMap, float startOne, float stopOne, float startTow, float stopTow)
+        {
+            return (valueToMap - startOne) / (stopOne - startOne) * (stopTow - startTow) + startTow;
+        }
+    }
+}
---
+++ ShootBird/ShootBird/ASCII_Art/Program.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Windows.Forms;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+
+namespace ASCII_Art
+{
+    internal class Program
+    {
+        private const double WIDTH_OFFSET = 1.5;
+        private const int MAX_WIDTH = 150;
+
+        [STAThread]
+        static void Main(string[] args)
+        {
+            var openFileDialog = new OpenFileDialog
+            {
+                Filter = "Images | *.bmp; *.png; *.jpg; *.JPEG; "
+            };
+
+            Console.WriteLine("Жмакни Enter для старта...\n");
+
+            while (true)
+            {
+                Console.ReadLine();
+
+                if (openFileDialog.ShowDialog() != DialogResult.OK)
+                    continue;
+
+                Console.Clear();
+
+                var bitmap = new Bitmap(openFileDialog.FileName);
+                bitmap = ResizeBitmap(bitmap);
+                bitmap.ToGray();
+
+                var converter = new BitmapToASCII(bitmap);
+                var rows = converter.Convert();
+
+                foreach(var row in rows)
+                    Console.WriteLine(row);
+
+                File.WriteAllLines("images.txt", rows.Select(r => new string(r)));
+
+
+                Console.SetCursorPosition(0, 0);
+            }
+        }
+
+        private static Bitmap ResizeBitmap(Bitmap bitmap)
+        {
+            var newHeight = bitmap.Height / WIDTH_OFFSET * MAX_WIDTH/ bitmap.Width;
+            if(bitmap.Width > MAX_WIDTH || bitmap.Height > newHeight)
+            {
+                bitmap = new Bitmap(bitmap, new Size(MAX_WIDTH, (int)newHeight));              
+            }
+            return bitmap;
+        }
+
+    }
+}
